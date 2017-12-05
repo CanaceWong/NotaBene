c09c7f7d27e366c298947483f704a8618c6298e3
Christine <splogy2001@hotmail.com>
2017-12-05 10:27:20 +0000

    Initial Commit

---
+++ NotaBene/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotaBene
+//
+//  Created by Christine Horrocks on 05/12/2017.
+//  Copyright © 2017 Christine Horrocks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
