b99d3fcf99e026062d4aec1aa078c2d253b29600
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-12-05 16:05:55 +0000

    new initial setup for login/signup with firebase

---
+++ NotaBene/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  NotaBene
+//
+//  Created by Olivia Beresford on 05/12/2017.
+//  Copyright © 2017 NotaBeneTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
