3da62f3b59637b365263372e2fc197699ea3fb30
Olivia Beresford <oliviaberesford@hotmail.co.uk>
2017-12-06 13:11:18 +0000

    restructured to include original note format, need to store note content as well as title in database

--- NotaBene/EntriesViewController.swift
+++ NotaBene/EntriesViewController.swift
@@ -1,31 +1,30 @@
 //
-//  ViewController2ViewController.swift
+//  Entries.swift
 //  NotaBene
 //
-//  Created by Olivia Beresford on 05/12/2017.
+//  Created by Olivia Beresford on 06/12/2017.
 //  Copyright © 2017 NotaBeneTeam. All rights reserved.
 //
 
 import UIKit
 import FirebaseAuth
 
-class ViewController2ViewController: UIViewController {
+class Entries: UIViewController {
     
     @IBAction func action(_ sender: UIButton) {
         try! Auth.auth().signOut()
         performSegue(withIdentifier: "logged2", sender: self)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print(Auth.auth().currentUser?.email)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
