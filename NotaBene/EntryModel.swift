63ba75ccbd625458404f62d598face7824580ef6
Christine <splogy2001@hotmail.com>
2017-12-07 14:44:29 +0000

    ViewcontrollerTabel view added and EntryModel added

---
+++ NotaBene/EntryModel.swift
@@ -0,0 +1,23 @@
+//
+//  EntryModel.swift
+//  NotaBene
+//
+//  Created by Christine Horrocks on 07/12/2017.
+//  Copyright © 2017 NotaBeneTeam. All rights reserved.
+//
+
+import Foundation
+
+class EntryModel {
+    
+    var id: String?
+    var entryTitle: String?
+    var entryContent: String?
+    
+    init(id: String?, entryTitle: String?, entryContent: String?) {
+        self.id = id;
+        self.entryTitle = entryTitle;
+        self.entryContent = entryContent;
+    }
+    
+}
